0e65582b9b969a67c40b3ef5ff7f180dec757347
Miroslav Tzarev <mtzarev@gmail.com>
2024-03-22 19:51:25 +0200

    Add files via upload

---
+++ ArraysAdvancedEqualNeighbors.js
@@ -0,0 +1,15 @@
+function equalNeighbors(arr) {
+    let counter = 0;
+    let totalArr = arr.join(` `)
+    let idx=0
+   for(let i=0; i<arr.length; i++){
+    idx=arr[i]
+   }
+    console.log(idx);
+}
+equalNeighbors([['2', '3', '4', '7', '0'],
+                ['4', '0', '5', '3', '4'],
+                ['2', '3', '5', '4', '2'],
+                ['9', '8', '7', '5', '4'],
+                ['9', '8', '7', '5', '4']]
+);
